5a1ee6aaae71c38d68986029ea00155638cf8112
Sikandar <sikandarbutt@hotmail.co.uk>
2024-06-23 16:03:01 +0100

    added all animals screen controller

--- src/main/java/com/ucen/vetclinicjavafx/vetclinicjavafx/app/ui/controllers/core/ScreenType.java
+++ src/main/java/com/ucen/vetclinicjavafx/vetclinicjavafx/app/ui/controllers/core/ScreenType.java
@@ -3,6 +3,7 @@ package com.ucen.vetclinicjavafx.vetclinicjavafx.app.ui.controllers.core;
 import com.ucen.vetclinicjavafx.vetclinicjavafx.app.ui.controllers.*;
 
 public enum ScreenType {
+    ALL_ANIMALS_SCREEN(AllAnimalsScreenController.class),
     ALL_HOSPITALS_SCREEN(AllHospitalsScreenController.class),
     CREATE_HOSPITAL_SCREEN(CreateHospitalScreenController.class),
     MY_BOOKINGS_SCREEN(MyBookingsScreenController.class),
